de209d9feae54ff5848d0d8ba5d912447f396db9
christovanqueiroz <106829027+christovanqueiroz@users.noreply.github.com>
2022-12-15 23:37:54 -0300

    modified:   index.html 	modified:   scripts.js 	modified:   styles.css

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// let topLeft = document.querySelector(".topLeft input").addEventListener("input", () => console.log(document.querySelector(".topLeft input").value));
+
+let topRight = document.querySelector(".topRight input").addEventListener("input", () => console.log(document.querySelector(".topLeft input").value));
+
+let bottomRight = document.querySelector(".bottomRight input").addEventListener("input", () => console.log(document.querySelector(".topLeft input").value));
+
+let bottomLeft = document.querySelector(".bottomLeft input").addEventListener("input", () => console.log(document.querySelector(".topLeft input").value));
+
+let topLeft = document.querySelector(".topLeft input").nodeList;
+
+console.log(topLeft)
