c1ca960b5944f0c47e4c2ee60eef563f88c5731c
christovanqueiroz <106829027+christovanqueiroz@users.noreply.github.com>
2022-12-17 22:12:04 -0300

    modified:   index.html 	modified:   scripts.js 	modified:   styles.css

--- scripts.js
+++ scripts.js
@@ -1,7 +1,38 @@
- function printValue() {
-// Get the value of the input field
-const topLeftValue = document.getElementById('topLeftValue').value;
+const topLeft = document.querySelector('#topLeft');
+const topRight = document.querySelector('#topRight');
+const bottomRight = document.querySelector('#bottomRight');
+const bottomLeft = document.querySelector('#bottomLeft');
 
-// Print the value to the textarea
-document.getElementById('inputResult').innerHTML += topLeftValue + '\n';
+const box = document.querySelector('.box');
+
+topLeft.addEventListener('input', function() {
+    // Get the value of the input element
+    let topLeftValue = topLeft.value;
+  
+    // Set the value of the border-radius
+    box.style.borderTopLeftRadius = topLeftValue + "px";
+})
+
+topRight.addEventListener('input', function() {
+    let topRightValue = topRight.value;
+    box.style.borderTopRightRadius = topRightValue + "px";
+})
+
+bottomRight.addEventListener('input', function() {
+    let bottomRightValue = bottomRight.value;
+    box.style.borderBottomRightRadius = bottomRightValue + "px";
+})
+
+bottomLeft.addEventListener('input', function() {
+    let bottomLeftValue = bottomLeft.value;
+    box.style.borderBottomLeftRadius = bottomLeftValue + "px";
+})
+
+function outputBorderRadius() {
+    let boxStyle = window.getComputedStyle(box);
+    let boxBorderRadius = boxStyle.getPropertyValue("border-radius")
+
+    document.getElementById("inputResult").value = boxBorderRadius;
 }
+
+outputBorderRadius()
